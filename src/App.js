9ed85694a7a07f57762f90dca91d25a03b8e433e
Niklas Hallberg <91habbe@gmail.com>
2019-03-06 17:15:10 +0100

    installed dotenv. created Login and Register components. added minor css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'bulma/css/bulma.css';
+import './App.css'
 import Navbar from "./Components/Navbar";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          {/* <Navbar isLoggedIn={this.state.isLoggedIn}/> */}
+          {/* <Route path="/"                 component={Home}/> */}
+          <Route path="/register"         component={Register}/>
+          <Route path="/login"            component={Login}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange = e => this.setState({[e.target.name]: e.target.value});
+
+    async onSubmit(e) {
+        e.preventDefault();
+        const { username, password } = this.state;
+        const login = await axios.post(`${process.env.REACT_APP_API_URL}/user/login`,
+            {
+                username,
+                password
+            }
+        );
+		console.log('TCL: Login -> onSubmit -> login', login)
+    }
+    
+    render() {
+        return (
+            <div>
+                <section class="section">
+                    <div className="container">
+                        <div className="columns is-centered">
+                            <div className="column is-one-third">
+                                <form onSubmit={this.onSubmit} className="form">
+                                    <label className="formLabel" htmlFor="username">Username</label>
+                                    <input onChange={this.onChange} type="text" name="username" className="input"/>
+                                    <label className="formLabel" htmlFor="password">Password</label>
+                                    <input onChange={this.onChange} type="password" name="password" className="input"/>
+                                    <div className="field is-grouped is-grouped-centered">
+                                    <button type="submit" className="formButton button is-centered is-info">Login</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Login;
